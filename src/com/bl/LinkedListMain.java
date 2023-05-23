26b618521dfc9d541098f9d55d85936603162445
lunainfotec <kirtimadan1997@gmail.com>
2023-05-23 21:39:10 +0530

    added : Uc1

---
+++ src/com/bl/LinkedListMain.java
@@ -0,0 +1,17 @@
+package com.bl;
+import java.util.LinkedList;
+
+public class LinkedListMain {
+
+    public static void main(String[] args) {
+
+        LinkedList<Integer> linkedList1 = new LinkedList<>();
+
+        linkedList1.add(56);
+        linkedList1.add(30);
+        linkedList1.add(70);
+
+        System.out.println("Linked List => " + linkedList1);
+    }
+
+}
